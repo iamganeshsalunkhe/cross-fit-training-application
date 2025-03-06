5d80c068335b95ac744926ccdf78781b980e39cd
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2025-03-06 13:07:33 +0530

    API for create, update and delete add

--- crossfit-wod-api/src/services/workoutService.js
+++ crossfit-wod-api/src/services/workoutService.js
@@ -1,4 +1,5 @@
 const Workout = require('../database/Workout');
+const {v4:uuid}= require('uuid');
 
 // name the service method same as the controller method
 
@@ -8,20 +9,32 @@ const getAllWorkouts = ()=>{
     return allWorkouts;
 };
 
-const getOneWorkout = ()=>{
-    return;
+const getOneWorkout = (workoutId)=>{
+    const workout = Workout.getOneWorkout(workoutId);
+    return workout;
+    
 };
 
-const createNewWorkout = ()=>{
-    return;
+const createNewWorkout = (newWorkout)=>{
+    const workoutToInsert = {
+        ...newWorkout,
+        id:uuid(),
+        createdAt:new Date().toLocaleString('en-US',{timeZone:"UTC"}),
+        updatedAt:new Date().toLocaleString('en-US',{timeZone:"UTC"})
+    };
+    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+
+    return createdWorkout;
 };
 
-const updateOneWorkout = ()=>{
-    return;
+const updateOneWorkout = (workoutId,changes)=>{
+    const updatedWorkout = Workout.updateOneWorkout(workoutId,changes);
+
+    return updatedWorkout;
 };
 
-const deleteOneWorkout = ()=>{
-    return;
+const deleteOneWorkout = (workoutId)=>{
+        Workout.deleteOneWorkout(workoutId);
 };
 
 module.exports = {
